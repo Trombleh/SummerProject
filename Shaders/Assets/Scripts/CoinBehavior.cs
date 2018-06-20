4089ce1ff0154228a877af92385ebd5d36ff0f40
Tyler <u1114642@utah.edu>
2018-06-20 15:02:18 -0600

    Updated UI. Updated Scene 1-1 adding prefabs.

---
+++ Shaders/Assets/Scripts/CoinBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        BoxCollider col = gameObject.GetComponent<BoxCollider>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
